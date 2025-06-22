49812b93bce7369e6e5a8bff46e312f43563d142
Giulia-Barros <130318382+Giulia-Barros@users.noreply.github.com>
2025-06-22 01:02:40 -0300

    Últimas atuliazações

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, CadastroView, LoginView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('login/', CadastroView.as_view(), name='cadastro'),
+    path('cadastro', LoginView.as_view(), name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,10 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class CadastroView(TemplateView):
+    template_name = 'cadastro.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
