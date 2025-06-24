b843251296e7277374b78c4e36e2a4a223ef93c0
Giulia-Barros <130318382+Giulia-Barros@users.noreply.github.com>
2025-06-24 17:56:14 -0300

    Crate Logout

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from .views import (
 HomeView, CadastroView, LoginView,
 ListaTarefasView, CriarTarefaView, AtualizarTarefaView,
@@ -10,6 +11,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('login/', LoginView.as_view(), name='login'),
     path('cadastro/', CadastroView.as_view(), name='cadastro'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 # Rotas de tarefas
     path('tarefas/', ListaTarefasView.as_view(), name='tarefas'),
     path('tarefas/criar/', CriarTarefaView.as_view(), name='criar_tarefa'),
@@ -17,3 +19,4 @@ urlpatterns = [
     path('tarefas/<int:pk>/deletar/', DeletarTarefaView.as_view(), name='deletar_tarefa'),
     path('tarefas/<int:pk>/alternar/', AlternarStatusTarefaView.as_view(), name='alternar_tarefa'),
 ]
+
